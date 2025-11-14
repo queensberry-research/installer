b5257e412286f3620bbddd9ba69976de8d9c8813
Derek Wan <d.wan@icloud.com>
2025-11-14 22:36:44 +0900

    2025-11-14 22:36:44 (Fri)  > DW-Mac  > derekwan

--- src/installer/constants.py
+++ src/installer/constants.py
@@ -1,14 +1,13 @@
 from __future__ import annotations
 
 from importlib.resources import files
-from typing import TYPE_CHECKING, cast
-
-if TYPE_CHECKING:
-    from pathlib import Path
-
+from pathlib import Path
+from typing import cast
 
 CONFIGS = cast("Path", files("configs").joinpath("z")).parent
 NONROOT = "nonroot"
+HOME_ROOT = Path("/root")
+HOME_NONROOT = Path("/home/nonroot")
 
 
-__all__ = ["CONFIGS", "NONROOT"]
+__all__ = ["CONFIGS", "HOME_NONROOT", "HOME_ROOT", "NONROOT"]
