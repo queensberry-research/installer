8fccecb55a23ca1591aefc7b719ce3ed5c607409
Derek Wan <d.wan@icloud.com>
2025-11-14 23:00:44 +0900

    2025-11-14 23:00:44 (Fri)  > DW-Mac  > derekwan

--- src/installer/settings.py
+++ src/installer/settings.py
@@ -3,23 +3,21 @@ from __future__ import annotations
 from collections.abc import Sequence
 from typing import ClassVar
 
-from pydantic import SecretStr
 from pydantic_settings import BaseSettings
-from utilities.pydantic_settings import PathLikeOrWithSection, load_settings
-from utilities.pydantic_settings_sops import SopsBaseSettings
+from utilities.pydantic_settings import (
+    CustomBaseSettings,
+    PathLikeOrWithSection,
+    load_settings,
+)
 
 from installer.constants import CONFIGS
 
 
-class _Settings(SopsBaseSettings):
+class _Settings(CustomBaseSettings):
     toml_files: ClassVar[Sequence[PathLikeOrWithSection]] = [CONFIGS / "config.toml"]
-    secret_files: ClassVar[Sequence[PathLikeOrWithSection]] = [
-        (CONFIGS / "secrets.json")
-    ]
 
     subnets: _Subnets
     downloads: _Downloads
-    pbs: SecretStr
 
 
 class _Downloads(BaseSettings):
